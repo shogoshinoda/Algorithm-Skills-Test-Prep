668ee8d223d6add11089ab7ddd3bff6b45ee2a1c
shogoshinoda <syogo1119@outlook.jp>
2023-11-14 21:59:18 +0900

    complete Hまとめ売り.py

--- Samples/Hまとめ売り.py
+++ Samples/Hまとめ売り.py
@@ -1,38 +1,61 @@
 N = int(input())
 C = list(map(int, input().split()))
 Q = int(input())
-ans = 0
-elist = list()
-for i in range(1, N+1):
-    if (i % 2 == 1):
-        elist.append(i-1)
-for _ in range(Q):
-    S = list(map(int, input().split()))
-    if (S[0] == 1):
-        x = S[1] - 1
-        a = S[2]
-        if (C[x] >= a):
-            C[x] -= a
-            ans += a
-    elif (S[0] == 2):
-        a = S[1]
-        b = True
-        for i in elist:
-            if not (C[i] >= a):
-                b = False
-        if (b):
-            for i in elist:
-                C[i] -= a
-                ans += a
-    else:
-        a = S[1]
-        b = True
-        for i in range(N):
-            if not (C[i] >= a):
-                b = False
-        if (b):
-            for i in range(N):
-                C[i] -= a
-                ans += a
 
-print(ans)
+sell = 0
+
+z = 0
+
+s = 0
+
+min_s_C = 10000000
+
+min_z_C = 10000000
+
+for i in range(0, N):
+    if i % 2 == 0:
+        min_s_C = min(C[i], min_s_C)
+    else:
+        min_z_C = min(C[i], min_z_C)
+
+for _ in range(Q):
+    query = list(map(int, input().split()))
+
+    if query[0] == 1:
+        x = query[1] - 1
+        a = query[2]
+
+        if x % 2 == 0:
+            card_x = C[x] - z - s
+        else:
+            card_x = C[x] - z
+
+        if card_x >= a:
+            C[x] -= a
+            sell += a
+
+            if x % 2 == 0:
+                min_s_C = min(C[x], min_s_C)
+            else:
+                min_z_C = min(C[x], min_z_C)
+
+
+    elif query[0] == 2:
+        a = query[1]
+
+        if min_s_C - s - z >= a:
+            s += a
+    
+    elif query[0] == 3:
+        a = query[1]
+
+        if min(min_s_C -s - z, min_z_C -z) >= a:
+            z += a
+
+for i in range(N):
+    if i % 2 == 0:
+        sell += s
+
+sell += z * N
+
+print(sell)
